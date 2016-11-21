c1c738cc1696f6c02b41461bb23d07bfb5d19479
Murdecai777 <ii01010101ii@gmail.com>
2016-11-22 02:22:44 +0300

    tests

---
+++ testRun.bat
@@ -0,0 +1,12 @@
+cd src\main\java
+java Application ..\..\..\input.txt
+
+java Application ..\..\..\input.txt -filter
+
+java Application ..\..\..\input2.txt
+
+java Application ..\..\..\input3.txt
+
+java Application ..\..\..\input3.txt -filter
+
+pause
